28df87942bd321456c7ef6055833117440eb5d4c
Alberto <albertosalmerontapia@gmail.com>
2025-11-09 10:32:57 +0100

    feat: Spinner loading

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingSpinnerComponent {}
