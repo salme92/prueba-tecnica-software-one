c597f3414abbac713e2db37d543746bf087c82ec
Alberto <albertosalmerontapia@gmail.com>
2025-11-09 11:57:26 +0100

    Feat: PWA + TEST.TS + Actualización

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  isDevMode,
+} from '@angular/core';
 import {
   provideRouter,
   withPreloading,
   PreloadAllModules,
 } from '@angular/router';
-import { appRoutes } from './app.routes';
 import {
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
+
+import { appRoutes } from './app.routes';
 import { GlobalErrorHandler } from './core/error-handling/global-error-handler';
 import { errorInterceptor } from './core/interceptors/error.interceptors';
 import { authInterceptor } from './core/interceptors/auth.interceptors';
@@ -16,12 +22,15 @@ import { authInterceptor } from './core/interceptors/auth.interceptors';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withPreloading(PreloadAllModules)),
-    provideHttpClient(
-      withInterceptors([authInterceptor, errorInterceptor])
-    ),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     {
       provide: ErrorHandler,
       useClass: GlobalErrorHandler,
     },
+    // ✅ Service Worker para PWA
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(), // solo en producción
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
